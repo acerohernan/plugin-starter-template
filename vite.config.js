96030a691411f3b8f91189ae075b0ce3e2b990e4
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-05-16 16:05:46 -0500

    fix: generate manifest in dist folder for support wp-vite plugin with vite 5

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,16 @@ import { defineConfig } from "vite";
 import { v4wp } from "@kucrut/vite-for-wp";
 
 export default defineConfig({
-  plugins: [v4wp({ input: "assets/js/index.js", outDir: "assets/dist" })],
+  plugins: [
+    v4wp({ input: "assets/js/index.js", outDir: "assets/dist" }),
+    {
+      name: "override-config",
+      config: () => ({
+        build: {
+          // ensure that manifest.json is not in ".vite/" folder
+          manifest: "manifest.json",
+        },
+      }),
+    },
+  ],
 });
