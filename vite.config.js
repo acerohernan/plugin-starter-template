dec20644c3866dfc087de1f024ed4577df71d748
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-05-16 16:25:59 -0500

    feat: cache busting added to vite bundle

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import { v4wp } from "@kucrut/vite-for-wp";
 
+const hash = Math.floor(Math.random() * 90000) + 10000;
+
 export default defineConfig({
   plugins: [
     v4wp({ input: "assets/js/index.js", outDir: "assets/dist" }),
@@ -8,10 +10,19 @@ export default defineConfig({
       name: "override-config",
       config: () => ({
         build: {
-          // ensure that manifest.json is not in ".vite/" folder
           manifest: "manifest.json",
         },
       }),
     },
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        sourcemap: false,
+        entryFileNames: `[name]` + hash + `.js`,
+        chunkFileNames: `[name]` + hash + `.js`,
+        assetFileNames: `[name]` + hash + `.[ext]`,
+      },
+    },
+  },
 });
